8c3b2f6069a24fb16f6916caeca907005028be1b
coihsan <110887888+coihsan@users.noreply.github.com>
2023-11-12 17:43:13 +0000

    Update 12 files

--- README.md
+++
@@ -1,3 +0,0 @@
-# zinc-starters-rgkhtp
-
-[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/coihsan/zinc-starters-rgkhtp)
---
+++ app/default/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <footer className="w-full">
+            <p className="text-center py-3">© Copyright 2023 - coihsan • All rights reserved.</p>
+        </footer>
+    )
+}
