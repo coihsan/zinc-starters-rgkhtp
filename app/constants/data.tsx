d2e62549d371f2d182aba0dba1fb3d45c1d06397
coihsan <110887888+coihsan@users.noreply.github.com>
2023-11-13 06:36:15 +0000

    Update 5 files

--- app/constants/data.tsx
+++ app/constants/data.tsx
@@ -1,14 +1,17 @@
 export const testimonial = [
     {
         name: "John Doe, CEO of Acme Corporation",
-        message: "I was looking for a web designer to help me create a new website for my business. I was impressed with the work of Zinc Studio, and I was confident that they could create a website that would meet my needs"
+        message: "I was looking for a web designer to help me create a new website for my business. I was impressed with the work of Zinc Studio, and I was confident that they could create a website that would meet my needs",
+        picture: "/user.svg"
     },
     {
         name: "Jane Doe, Owner of Jane's Boutique",
-        message: "Zinc Studio met all of my criteria. They were able to understand my needs quickly, and they were able to develop a website that met my expectations. The website is well-designed, easy to use, and it is performing well"
+        message: "Zinc Studio met all of my criteria. They were able to understand my needs quickly, and they were able to develop a website that met my expectations. The website is well-designed, easy to use, and it is performing well",
+        picture: "/user.svg"
     },
     {
         name: "Susan Smith, Owner of Smith's Bakery",
-        message: "I am very happy with the results of Zinc Studio's digital marketing services. My business has grown significantly, and I am confident that Zinc Studio will continue to help me grow my business in the future."
+        message: "I am very happy with the results of Zinc Studio's digital marketing services. My business has grown significantly, and I am confident that Zinc Studio will continue to help me grow my business in the future.",
+        picture: "/user.svg"
     },
 ]
--- app/default/Testimonial.tsx
+++ app/default/Testimonial.tsx
@@ -1,13 +1,18 @@
 import {testimonial} from '@/app/constants/data'
+import Image from '@/node_modules/next/image'
 export default function Testimoni(){
     return(
-        <section className="max-w-screen-xl mx-auto px-[1rem] w-full grid grid-cols-3 gap-3">
+        <section className="max-w-screen-xl mx-auto px-[1rem] py-[7rem] ">
+            <h1 className='textHeading'>Testimoni</h1>
+            <div className='w-full grid grid-cols-3 gap-3 max-[600px]:flex max-[600px]:overflow-scroll'>
             {testimonial.map((person) =>(
-            <div className='flex flex-col bg-zinc-200 rounded-md border border-zinc-300 p-5'>
-                <h3>{person.name}</h3>
-                <span>{person.message}</span>
+            <div className='flex flex-col items-center gap-3 bg-zinc-50 rounded-md border border-zinc-300 p-9 max-[600px]:flex-1 max-[600px]:min-w-full'>
+                <Image src={person.picture} width={40} height={40} alt="user"/>
+                <span className='text-center'>{person.message}</span>
+                <h3 className='font-bold text-center'>{person.name}</h3>
             </div>
             ))}
+            </div>
         </section>
     )
 }
