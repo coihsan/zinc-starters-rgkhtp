b07b76dc7af411e756b6645e4660548f3217dce5
coihsan <110887888+coihsan@users.noreply.github.com>
2023-11-13 18:54:55 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from './default/Hero'
 import PriceCard from './default/PriceCard'
 import Testimonial from './default/Testimonial'
 import Header from './default/Header'
-import GoogleMaps from './u/Header'
+import GoogleMaps from './ui/GoogleMaps'
 
 export default function Home() {
   return (
