a15fcee4b791ec13a0363b629d19a1a3db2053b2
coihsan <110887888+coihsan@users.noreply.github.com>
2023-11-13 10:52:38 +0000

    Update price.tsx and PriceCard.tsx

--- app/constants/price.tsx
+++ app/constants/price.tsx
@@ -1,20 +1,41 @@
 export const package1 = [
     {
         name: "Standard",
-        package: ["Items 1", "Items 2", "Items 3", "Items 4", "Items 5"],
+        desc: "Lorem ipsum dolor sit amet",
+        package: [
+            {item: "Items 1"}, 
+            {item: "Items 2"}, 
+            {item: "Items 3"}, 
+            {item: "Items 4"}, 
+            {item: "Items 5"}, 
+        ],
         basicprice: "4.000.000",
         promoprice: "3.500.000"
     },
     {
         name: "Basic",
-        package: ["Items 1", "Items 2", "Items 3", "Items 4", "Items 5"],
-        basicprice: "4.000.000",
-        promoprice: "3.500.000"
+        desc: "Lorem ipsum dolor sit amet",
+        package: [
+            {item: "Items 1"}, 
+            {item: "Items 2"}, 
+            {item: "Items 3"}, 
+            {item: "Items 4"}, 
+            {item: "Items 5"}, 
+        ],
+        basicprice: "4.500.000",
+        promoprice: "4.000.000"
     },
     {
         name: "Premium",
-        package: ["Items 1", "Items 2", "Items 3", "Items 4", "Items 5"],
-        basicprice: "4.000.000",
-        promoprice: "3.500.000"
+        desc: "Lorem ipsum dolor sit amet",
+        package: [
+            {item: "Items 1"}, 
+            {item: "Items 2"}, 
+            {item: "Items 3"}, 
+            {item: "Items 4"}, 
+            {item: "Items 5"}, 
+        ],
+        basicprice: "5.000.000",
+        promoprice: "4.500.000"
     },
 ]
--- app/default/PriceCard.tsx
+++ app/default/PriceCard.tsx
@@ -1,7 +1,29 @@
-export default function PriceCard(){
+import {package1} from '@/app/constants/price'
+import { createDeflate } from 'zlib'
+import React from 'react';
+
+const PriceCard: React.FC = () =>{
     return(
         <section className="max-w-screen-xl mx-auto px-[1rem] py-[7rem]">
-            <h1>Pricecard</h1>
+            <h1 className='textHeading'>Pricecard</h1>
+            <div className='grid grid-cols-3 max-[600px]:grid-cols-1 gap-3'>
+                {package1.map((card, index) => (
+                    <div key={index} className="bg-white p-8 rounded-xl border border-zinc-300">
+                        <h2 className='font-bold text-center text-2xl'>{card.name}</h2>
+                        <div className=' flex flex-col items-center text-center'>
+                            <span>{card.basicprice}</span>
+                            <span className='w-full rounded-xl p-2 bg-zinc-100 text-3xl font-bold'>{card.promoprice}</span>
+                        </div>
+                        <p className='text-center'>{card.desc}</p>
+                        <ul className='grid gap-3'>
+                            {card.package.map((item, itemIndex) => (
+                                <li className='text-center' key={itemIndex}>{item.item}</li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </div>
         </section>
     )
 }
+export default PriceCard
