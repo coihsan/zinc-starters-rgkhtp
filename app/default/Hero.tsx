4428becad8dd2e4674538f9a1bd48aaca64d0bf8
coihsan <110887888+coihsan@users.noreply.github.com>
2023-11-15 18:56:09 +0000

    Update Hero.tsx

--- app/default/Hero.tsx
+++ app/default/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero(){
     return (
         <section className="w-full mx-auto max-w-screen-xl px-[1rem] max-[600px]:pt-[2rem] pt-[2rem] gap-5 flex max-[600px]:flex-col-reverse items-center justify-between">
             <div className="flex-[1_1_0%] max-w-xl flex flex-col gap-3 max-[600px]:items-center">
-                <h1 className="text-5xl max-[600px]:text-4xl font-bold max-[600px]:text-center">This is your text marketing campaign on heading tag.</h1>
+                <h1 className="text-5xl max-[600px]:text-4xl font-bold max-[600px]:text-center">Free template for creating websites with Next.js + Tailwind CSS</h1>
                 <p className="max-[600px]:text-center pb-[1rem]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                 <CallUs />
             </div>
